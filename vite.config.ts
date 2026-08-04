450ee6d4a574f02ce1b915740cd4cd51e2dd6d56
robertsbaer <robertsbaer@yahoo.com>
2026-08-04 17:25:25 +0200

    feat: Add CNAME for custom domain

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { fileURLToPath, URL } from 'node:url';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/90days180rule/',
+  base: '/',
   plugins: [react()],
   resolve: {
     alias: {
